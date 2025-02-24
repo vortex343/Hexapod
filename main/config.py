778f0475016c225bcade5f01449859266127c3b7
Vortex0611 <elias.raunig@gmail.com>
2025-02-24 21:24:46 +0100

    new servo pins

--- main/config.py
+++ main/config.py
@@ -11,13 +11,13 @@ csv_file_path = 'main/data/8bitDo.csv'
 
 legs = {
     # 'leg_name':   [Position,      servo_pins,     lengths,        joint_offsets,      inverted]
-    'front_left':   [[5.5, -4, 5],  [5, 6, 7],      [L1, L2, L3],   [300, 120, 350],    [False, True, False]],
-    'front_right':  [[5.5, 4, 5],   [13, 14, 15],   [L1, L2, L3],   [15, 10, 140],      [False, False, True]],
+    'front_left':   [[5.5, -4, 5],  [7,13,15],      [L1, L2, L3],   [300, 120, 350],    [False, True, False]],
+    'front_right':  [[5.5, 4, 5],   [4,12,14],   [L1, L2, L3],   [15, 10, 140],      [False, False, True]],
 
-    'middle_left':  [[0, -5, 5],    [4, 3],         [L1, 24],   [65, 55 ],       [True, True]],
-    'middle_right': [[0, 5, 5],     [12, 11],       [L1, 24],   [65, 80],           [False, False]],
+    'middle_left':  [[0, -5, 5],    [6,11],         [L1, 24],   [65, 55 ],       [True, True]],
+    'middle_right': [[0, 5, 5],     [3,10],       [L1, 24],   [65, 80],           [False, False]],
 
-    'back_left':    [[-7, -4, 3], [2, 1, 0],        [L1, L2, L3],   [195, 120, 350],    [False, True, False]],
-    'back_right':   [[-7, 4, 3],  [10, 9, 8],       [L1, L2, L3],   [120, 10, 140],     [False, False, True]]
+    'back_left':    [[-7, -4, 3], [5,9,0],        [L1, L2, L3],   [195, 120, 350],    [False, True, False]],
+    'back_right':   [[-7, 4, 3],  [2,8,1],       [L1, L2, L3],   [120, 10, 140],     [False, False, True]]
 }
 
